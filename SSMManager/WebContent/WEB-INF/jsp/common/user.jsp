14d7dbf65bfbcccad2c74531c07084b5772b3a81
梁城月 <2524499314@qq.com>
2019-04-15 23:36:22 +0800

    用户注册

---
+++ SSMManager/WebContent/WEB-INF/jsp/common/user.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<h2>欢迎业主：${user.name}</h2>
+</body>
+</html>
--- SSMManager/src/com/ssm/controller/CommonController.java
+++ SSMManager/src/com/ssm/controller/CommonController.java
@@ -1,17 +1,29 @@
 package com.ssm.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ssm.pojo.User;
 
 @Controller
 @RequestMapping("/common")
 public class CommonController {
 	@RequestMapping("/toFrame")
-	public String toFrame() {
-		return "jsp/frame";
+	public String toFrame(HttpServletRequest request) {
+		User user = (User) request.getSession().getAttribute("user");
+		if (user == null) {
+			return "redirect:/user/toLogin";
+		}
+		return "jsp/common/frame";
 	}
-	@RequestMapping("/toDataServer")
-	public String toDataServer() {
-		return "jsp/data_server";
+
+	@RequestMapping("/toUser")
+	public String toUser() {
+		return "jsp/common/user";
+	}
+	@RequestMapping("/toAdmin")
+	public String toAdmin() {
+		return "jsp/common/admin";
 	}
 }
